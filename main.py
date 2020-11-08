a24ec43cadd6e9e51bde3e5e30e1647f3afd1619
debaser19 <steve@stoveld.xyz>
2020-11-08 16:59:43 -0500

    added flask app to main, moved balance checker, added chart.js

--- main.py
+++ main.py
@@ -1,66 +1,20 @@
-import creds
-import os
-from selenium import webdriver
-import time
 import sqlite3
 import datetime
+import time
+from flask import Flask
+from flask import render_template
 
 
-def get_account_balance():
-    if os.name == 'nt':     # if windows
-        driver_path = '.\\driver\\chromedriver.exe'
-    else:   # if osx/linux
-        driver_path = './driver/chromedriver'
-
-    # set driver, open and navigate to login
-    driver = webdriver.Chrome(executable_path=driver_path)
-    driver.get('https://manage.tastyworks.com/index.html#/login')
-
-    # enter login
-    tw_user_field = driver.find_element_by_id('ember881')
-    tw_user_field.send_keys(creds.creds['tw_user'])
-
-    # enter password
-    tw_pass_field = driver.find_element_by_id('ember883')
-    tw_pass_field.send_keys(creds.creds['tw_pass'])
-
-    # click the login button
-    driver.find_element_by_class_name('login-button').click()
-
-    # navigate to balances
-    driver.get('https://manage.tastyworks.com/index.html#/accounts/balances')
-
-    # give a few seconds to let page load
-    time.sleep(3)
-
-    # get account balance
-    balance = driver.find_element_by_class_name('balance').text
-    # drop the $ from string
-    balance = balance[1:]
-
-    # return the balance string as a float without commas
-    return float(balance.replace(',', ''))
+app = Flask(__name__)
 
 
-def write_db():
-    # set parameters to use as insertion values
-    params = (datetime.datetime.now(), current_balance)
-    # connect to the db, create if it does not exist
-    conn = sqlite3.connect('database/balance_db.db')
-    c = conn.cursor()
-
-    # create table if it doesn't exist
-    c.execute('''CREATE TABLE IF NOT EXISTS balance
-    (timestamp datetime, balance real)''')
-
-    # write balance to db
-    c.execute('INSERT INTO balance VALUES (?, ?)', params)
-
-    # commit to db
-    conn.commit()
-    conn.close()
+@app.route("/simple_chart")
+def chart():
+    legend = 'Monthly Data'
+    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    values = [10, 9, 8, 7, 6, 4, 7, 8]
+    return render_template('chart.html', values=values, labels=labels, legend=legend)
 
 
-if __name__ == '__main__':
-    current_balance = get_account_balance()
-    write_db()
+if __name__ == "__main__":
+    app.run(debug=True)
