bf7149d3d81203cddada56615ec6d8c140bdf1d0
debaser19 <steve@stoveld.xyz>
2021-01-27 23:44:33 -0500

    implemented flask, account balance and positions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # stonks-only-go-up
 A tool to keep a historical record of my tendies
+
+## to do
+* set up influx db to log account balance on regular intervals
+* implement a charting library to display balance history and portfolio diversity or whatever else
+* set up each position ticker as a link to show ticker's chart and any news (pull from yahoo api?)
+
+## charting libraries
+* https://gionkunz.github.io/chartist-js/
+* https://epochjs.github.io/epoch/
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tda import auth, client
-from flask import render_template
-# import Flask
+from flask import Flask, render_template
 import json
 import config
 
@@ -8,6 +7,8 @@ import config
 api_key = config.api_key
 token_path = config.token_path
 redirect_uri = config.redirect_uri
+
+app = Flask(__name__)
 
 
 def connectToApi():
@@ -22,7 +23,6 @@ def connectToApi():
                 driver, api_key, redirect_uri, token_path)
 
 
-
 def getAccountInfo():
     c = connectToApi()
     r = c.get_account('455720137', fields=c.Account.Fields.POSITIONS)
@@ -31,9 +31,78 @@ def getAccountInfo():
     return account_info
 
 
-def getNetLiq():
-    return getAccountInfo()['securitiesAccount']['initialBalances']['liquidationValue']
+def getBalances():
+    account_info = getAccountInfo()
+    balance_dict = {
+        'net_liq': account_info['securitiesAccount']['currentBalances']['liquidationValue'],
+        'cash': account_info['securitiesAccount']['currentBalances']['availableFunds'],
+        'buying_power': account_info['securitiesAccount']['currentBalances']['buyingPower'],
+        'margin_balance': account_info['securitiesAccount']['currentBalances']['marginBalance']
+    }
+
+    return balance_dict
+
+
+@app.route('/')
+def index():
+    account_info = getAccountInfo()
+
+    # account balances
+    balance_dict = {
+        'net_liq': account_info['securitiesAccount']['currentBalances']['liquidationValue'],
+        'cash': account_info['securitiesAccount']['currentBalances']['availableFunds'],
+        'buying_power': account_info['securitiesAccount']['currentBalances']['buyingPower'],
+        'margin_balance': account_info['securitiesAccount']['currentBalances']['marginBalance']
+    }
+
+    print(balance_dict)
+
+    # positions
+    # create empty lists for stocks and options
+    stock_positions = []
+    option_positions = []
+    positions_list = account_info['securitiesAccount']['positions']
+    
+    for position in positions_list:
+
+        # check the asset type
+        if position['instrument']['assetType'] == 'EQUITY': # asset type is shares
+
+            stock_position_dict = {
+                'asset_type': position['instrument']['assetType'],
+                'ticker': position['instrument']['symbol'],
+                'quantity': position['longQuantity'] - position['shortQuantity'],
+                'trade_price': position['averagePrice'],
+                'current_price': 'fixme',
+                'pl_day': position['currentDayProfitLoss'],
+                'pl_day_percent': position['currentDayProfitLossPercentage'],
+                'pl_open': 'fixme'
+            }
+
+            stock_positions.append(stock_position_dict)
+
+        # asset type is an option, determine if cal/put
+        else:
+
+            option_position_dict = {
+                'asset_type': position['instrument']['putCall'],
+                'ticker': position['instrument']['underlyingSymbol'],
+                'description': position['instrument']['description'],
+                'quantity': position['longQuantity'] - position['shortQuantity'],
+                'trade_price': position['averagePrice'],
+                'current_price': 'fixme',
+                'pl_day': position['currentDayProfitLoss'],
+                'pl_day_percent': position['currentDayProfitLossPercentage'],
+                'pl_open': 'fixme'
+            }
+
+            option_positions.append(option_position_dict)
+    
+    return render_template('index.html',
+    balance_dict=balance_dict,
+    stock_positions=stock_positions,
+    option_positions=option_positions)
 
 
 if __name__ == '__main__':
-    print(json.dumps(getAccountInfo(), indent=4))
+    app.run(debug=True)
