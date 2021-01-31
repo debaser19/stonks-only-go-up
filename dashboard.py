7f8fd6faf004d7da3113b991ac22a30dac1e31ba
Steve Stoveld <steve@stoveld.xyz>
2021-01-31 21:15:49 +0000

    timeframe picker

--- dashboard.py
+++ dashboard.py
@@ -14,13 +14,29 @@ import pandas as pd
 from influxdb import InfluxDBClient, DataFrameClient
 import config
 import time
+import pytz
+import emoji
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    dcc.Graph(id='graph', animate=True),
+    dcc.RadioItems(
+        options = [
+            {'label': '1D', 'value': '1d'},
+            {'label': '7D', 'value': '7d'},
+            {'label': '30D', 'value': '30d'},
+            {'label': '3M', 'value': '90d'},
+            {'label': '6M', 'value': '180d'},
+            {'label': '1Y', 'value': '365d'},
+            {'label': 'ALL', 'value': 'all'}
+        ],
+        id = 'timeframe',
+        value = '1d',
+        labelStyle = {'display': 'inline-block'}
+    ),
+    dcc.Graph(id='graph', animate=False),
     dcc.Interval(
         id = 'my-interval',
         interval = 5 * 1000,
@@ -28,36 +44,55 @@ app.layout = html.Div([
     )
 ])
 
+
 # callback for updating on interval
 @app.callback(
     Output('graph', 'figure'),
-    [Input('my-interval', 'n_intervals')]
+    [Input('my-interval', 'n_intervals'),
+    Input('timeframe', 'value')]
 )
-def updateGraph(num):
+def updateGraph(num, timeframe):
     # connect to influx and import to df
+    if timeframe == '1d':
+        time_interval = '30s'
+    elif timeframe == '7d':
+        time_interval = '5m'
+    elif timeframe == '30d':
+        time_interval = '2h'
+    elif timeframe == '90d':
+        time_interval = '1d'
+    elif timeframe == '180d':
+        time_interval = '3d'
+    elif timeframe == '365d':
+        time_interval = '7d'
+    else:
+        time_interval = '7d'
+
     client = DataFrameClient('localhost', 8086, config.influxdb_user, config.influxdb_pass, 'balance_history')
-    query = 'select * from balance where time > now() - 24h'
+    query = f'select time, mean(value) as value from balance where time > now() - {timeframe} GROUP BY time({time_interval})'
     results = client.query(query)
     df = results['balance']
     df['timestamp'] = df.index
-    df['timestamp'] = df['timestamp'].tz_convert('US/Eastern')
+    eastern = pytz.timezone('US/Eastern')
+    df['timestamp'] = df['timestamp'].tz_convert(eastern)
 
     # TODO: Need to fix the auto updating. It seems to updat ethe line data, but not the axis data
 
     data =[
         go.Scatter(
             y = df.value,
-            x = df.timestamp,
+            x = df.timestamp.tz_convert(eastern),
             mode = 'lines'
         )
     ]
 
     layout = go.Layout(
-        title = 'Sweet Tendies'
+        title = emoji.emojize('AMC MOON :rocket: :rocket: :rocket: :rocket: :full_moon: :full_moon: :full_moon:')
     )
     fig = go.Figure(data=data, layout=layout)
 
     # print(client.query('select last("value") as value from balance'))
+    # print(df)
 
     return fig
 
