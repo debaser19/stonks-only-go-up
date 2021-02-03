25f324533e928067f2b42006b8ae00aad5082b43
Steve Stoveld <steve@stoveld.xyz>
2021-02-02 22:34:11 -0500

    added positions tables

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,7 @@ import plotly.graph_objects as go
 import dash             
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 from dash.dependencies import Input, Output
 # import dash
 # import dash_core_components as dcc
@@ -16,6 +17,13 @@ import config
 import time
 import pytz
 import emoji
+from tda import auth, client
+import config
+
+
+api_key = config.api_key
+token_path = config.token_path
+redirect_uri = config.redirect_uri
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -25,6 +33,7 @@ current_balance = 0
 app.layout = html.Div([
     html.H1(
         id = 'balance-h1',
+        # TODO: Need to pull in other balance info such as margin and cash values
         children = f'Current Balance: {current_balance}'),
     dcc.RadioItems(
         options = [
@@ -40,6 +49,37 @@ app.layout = html.Div([
         labelStyle = {'display': 'inline-block'}
     ),
     dcc.Graph(id='graph', animate=False),
+    html.H1('Positions'),
+    html.H3('Stonks'),
+    # TODO: Need to dymanically pull in headers for tables
+    dash_table.DataTable(
+        id = 'stocks_table',
+        columns = [
+            {'name': 'Asset Type', 'id': 'asset_type'},
+            {'name': 'Ticker', 'id': 'ticker'},
+            {'name': 'Quantity', 'id': 'quantity'},
+            {'name': 'Trade Price', 'id': 'trade_price'},
+            {'name': 'Current Price', 'id': 'current_price'},
+            {'name': 'P/L Day', 'id': 'pl_day'},
+            {'name': 'P/L Day %', 'id': 'pl_day_percent'},
+            {'name': 'P/L Open', 'id': 'pl_open'}
+        ]
+    ),
+    html.H3('Options'),
+    dash_table.DataTable(
+        id = 'options_table',
+        columns = [
+            {'name': 'Asset Type', 'id': 'asset_type'},
+            {'name': 'Ticker', 'id': 'ticker'},
+            {'name': 'Description', 'id': 'description'},
+            {'name': 'Quantity', 'id': 'quantity'},
+            {'name': 'Trade Price', 'id': 'trade_price'},
+            {'name': 'Current Price', 'id': 'current_price'},
+            {'name': 'P/L Day', 'id': 'pl_day'},
+            {'name': 'P/L Day %', 'id': 'pl_day_percent'},
+            {'name': 'P/L Open', 'id': 'pl_open'}
+        ]
+    ),
     dcc.Interval(
         id = 'my-interval',
         interval = 5 * 1000,
@@ -48,7 +88,7 @@ app.layout = html.Div([
 ])
 
 
-# update balance callback
+# Update balance on interval
 @app.callback(
     Output('balance-h1', 'children'),
     [Input('my-interval', 'n_intervals')]
@@ -63,7 +103,7 @@ def updateBalance(num):
     return html.H1(id = 'balance-h1', children = f'Current balance: {current_balance}')
 
 
-# callback for updating on interval
+# Update graph on interval
 @app.callback(
     Output('graph', 'figure'),
     [Input('my-interval', 'n_intervals'),
@@ -101,13 +141,105 @@ def updateGraph(num, timeframe):
     ]
 
     layout = go.Layout(
-        title = emoji.emojize('AMC MOON :rocket: :rocket: :rocket: :rocket: :full_moon: :full_moon: :full_moon:')
+        title = emoji.emojize('AMC ded? :(')
     )
     fig = go.Figure(data=data, layout=layout)
 
     fig.update_xaxes(tickformat = '%I:%M %p\n%x')
 
     return fig
+
+
+# Update stock positions on interval
+@app.callback(
+    Output('stocks_table', 'data'),
+    [Input('my-interval', 'n_intervals')]
+)
+def updateStockPositions(num):
+    stock_positions = getPositions()[0]
+    df = pd.DataFrame(stock_positions)
+    print(df.to_dict('data'))
+
+    return df.to_dict('records')
+
+
+# Update option positions on interval
+@app.callback(
+    Output('options_table', 'data'),
+    [Input('my-interval', 'n_intervals')]
+)
+def updateOptionPositionss(num):
+    options_positions = getPositions()[1]
+    df = pd.DataFrame(options_positions)
+    print(df.to_dict('data'))
+
+    return df.to_dict('records')
+
+
+def connectToApi():
+    # generate token
+    try:
+        c = auth.client_from_token_file(token_path, api_key)
+        return c
+    except FileNotFoundError:
+        from selenium import webdriver
+        with webdriver.Chrome() as driver:
+            c = auth.client_from_login_flow(
+                driver, api_key, redirect_uri, token_path)
+
+
+def getAccountInfo():
+    c = connectToApi()
+    r = c.get_account('455720137', fields=c.Account.Fields.POSITIONS)
+    account_info = r.json()
+
+    return account_info
+
+
+def getPositions():
+    account_info = getAccountInfo()
+    stock_positions = []
+    option_positions = []
+    positions_list = account_info['securitiesAccount']['positions']
+
+    # TODO: Need to add in current price and p/l open values
+    
+    for position in positions_list:
+
+        # check the asset type
+        if position['instrument']['assetType'] == 'EQUITY': # asset type is shares
+
+            stock_position_dict = {
+                'asset_type': position['instrument']['assetType'],
+                'ticker': position['instrument']['symbol'],
+                'quantity': position['longQuantity'] - position['shortQuantity'],
+                'trade_price': position['averagePrice'],
+                'current_price': 'fixme',
+                'pl_day': position['currentDayProfitLoss'],
+                'pl_day_percent': position['currentDayProfitLossPercentage'],
+                'pl_open': 'fixme'
+            }
+
+            stock_positions.append(stock_position_dict)
+
+        # asset type is an option, determine if cal/put
+        else:
+
+            option_position_dict = {
+                'asset_type': position['instrument']['putCall'],
+                'ticker': position['instrument']['underlyingSymbol'],
+                'description': position['instrument']['description'],
+                'quantity': position['longQuantity'] - position['shortQuantity'],
+                'trade_price': position['averagePrice'],
+                'current_price': 'fixme',
+                'pl_day': position['currentDayProfitLoss'],
+                'pl_day_percent': position['currentDayProfitLossPercentage'],
+                'pl_open': 'fixme'
+            }
+
+            option_positions.append(option_position_dict)
+    
+    return stock_positions, option_positions
 
 
 if __name__ == '__main__':
