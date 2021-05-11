be7bffe4355b88d9d949a661f33f191d57627c90
debaser19 <steve@stoveld.xyz>
2021-05-10 22:51:35 -0400

    added Options and sidebar timeframe selector

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ def connect_to_api():
     pass
 
 
-def get_balance_history():
+def get_balance_history(timeframe):
     client = DataFrameClient(config.influx_host, 8086, config.influxdb_user, config.influxdb_pass, 'balance_history')
-    query = f'select time, NetLiq from balance where time > now() - 1d'
+    query = f'select time, NetLiq from balance where time > now() - {timeframe}'
     results = client.query(query)
-    df = pd.DataFrame(results['balance'])
-    df['NetLiq'] = pd.to_numeric(df['NetLiq'], downcast='float')
+    try:
+        df = pd.DataFrame(results['balance'])
+        df['NetLiq'] = pd.to_numeric(df['NetLiq'], downcast='float')
+    except KeyError as e:
+        st.write(f'No plot points in selected timeframe: {timeframe}')
 
     return df
     # return results['balance']
@@ -30,21 +33,25 @@ def get_current_balances():
     return results['balance']
 
 
-def get_current_positions():
+def get_current_positions(position):
     client = DataFrameClient(config.influx_host, 8086, config.influxdb_user, config.influxdb_pass, 'balance_history')
-    query = 'select time, Positions from balance group by * order by time desc limit 1'
+    query = f'select time, {position} from balance group by * order by time desc limit 1'
     results = client.query(query)
     
-    positions = results['balance'].iloc[0]['Positions']
+    positions = results['balance'].iloc[0][position]
 
     # need to return the string representation of a list as a list
     return ast.literal_eval(positions)
 
 
 def main():
+    pos_selectbox = st.sidebar.selectbox(
+    "Select a timeframe",
+    ("1h", "12h", "24h", "7d", "30d")
+    )
     st.title("Tendies")
     st.dataframe(get_current_balances())
-    chart_data = get_balance_history()
+    chart_data = get_balance_history(pos_selectbox)
     chart_data['Date'] = chart_data.index
     # chart_data.index = chart_data.index.tz_convert('US/Eastern')
 
@@ -87,7 +94,10 @@ def main():
     # )
 
     st.plotly_chart(fig, use_container_width=True)
-    st.table(get_current_positions())
+    st.write('Stocks')
+    st.table(get_current_positions('Positions'))
+    st.write('Options')
+    st.table(get_current_positions('Options'))
     print(chart_data)
 
 
