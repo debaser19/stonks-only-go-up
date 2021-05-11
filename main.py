f1617c531b0e3bfb86c5e0ccae3382c224bde59d
Steve Stoveld <steve@stoveld.xyz>
2021-05-10 22:12:08 -0400

    updated  graph

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import config
 import ast
 import plotly.express as px
+import plotly.graph_objects as go
 
 
 def connect_to_api():
@@ -12,7 +13,7 @@ def connect_to_api():
 
 def get_balance_history():
     client = DataFrameClient(config.influx_host, 8086, config.influxdb_user, config.influxdb_pass, 'balance_history')
-    query = f'select time, NetLiq from balance where time > now() - 24h'
+    query = f'select time, NetLiq from balance where time > now() - 1d'
     results = client.query(query)
     df = pd.DataFrame(results['balance'])
     df['NetLiq'] = pd.to_numeric(df['NetLiq'], downcast='float')
@@ -47,7 +48,26 @@ def main():
     chart_data['Date'] = chart_data.index
     # chart_data.index = chart_data.index.tz_convert('US/Eastern')
 
-    fig = px.line(chart_data, x = 'Date', y = 'NetLiq')
+    # go
+    data =[
+        go.Scatter(
+            y = chart_data.NetLiq,
+            x = chart_data.Date,
+            mode = 'lines',
+            line = {'color': 'yellow'}
+        )
+    ]
+
+    layout = go.Layout(
+        uirevision = data,
+        paper_bgcolor = 'rgb(14, 17, 23)',
+        plot_bgcolor = 'rgb(14, 17, 23)',
+        font = {'color': 'white'},
+        hovermode = 'x'
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+
     fig.update_xaxes(
         tickformat = '%I:%M %p\n%x',
         rangebreaks = [
@@ -55,6 +75,17 @@ def main():
             dict(bounds = [20, 4], pattern = 'hour')
         ]
     )
+
+    #px
+    # fig = px.line(chart_data, x = 'Date', y = 'NetLiq')
+    # fig.update_xaxes(
+    #     tickformat = '%I:%M %p\n%x',
+    #     rangebreaks = [
+    #         dict(bounds = ['sat', 'mon']),
+    #         dict(bounds = [20, 4], pattern = 'hour')
+    #     ]
+    # )
+
     st.plotly_chart(fig, use_container_width=True)
     st.table(get_current_positions())
     print(chart_data)
